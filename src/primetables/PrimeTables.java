f066aa5555e3595658c0d20bdbb9bec256e60c8e
TwoRice <2rimagowan@gmail.com>
2017-02-26 23:10:45 +0000

    Implemented method for calculating multiplication table

--- src/primetables/PrimeTables.java
+++ src/primetables/PrimeTables.java
@@ -12,14 +12,26 @@ package primetables;
 public class PrimeTables {
 
     public static void main(String[] args) {
-        int [] primes = PrimeGenerator.generatePrimes(100);
+        int [] primes = PrimeGenerator.generatePrimes(3);
         int[][] primeTable = generatePrimeTable(primes);
         prettyPrintTable(primeTable);
     }
     
     public static int[][] generatePrimeTable(int[] primes){
         int[][] primeTable = new int[primes.length+1][primes.length+1];
+        primeTable[0][0] = 1;
         
+        for(int i = 0; i < primes.length; i++){
+            primeTable[i+1][0] = primes[i];
+            primeTable[0][i+1] = primes[i];
+        }
+        
+        for(int i = 1; i <= primes.length; i++){
+            for(int j = 1; j <= primes.length; j++){
+                primeTable[i][j] = primes[i-1] * primes[j-1];
+            }
+        }
+           
         return primeTable;
     }
     
