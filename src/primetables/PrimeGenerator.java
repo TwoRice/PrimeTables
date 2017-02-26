b8e606b7db1f9bff5b2449a0062eb6296dab40ea
TwoRice <2rimagowan@gmail.com>
2017-02-26 21:42:44 +0000

    add basic logic for iterating through numbers then adding it to the array if its a prime

--- src/primetables/PrimeGenerator.java
+++ src/primetables/PrimeGenerator.java
@@ -8,15 +8,21 @@ package primetables;
 
 public class PrimeGenerator {
 
-        
     public static int[] generatePrimes(int n){
         int[] primes = new int[n+1];
         primes[0] = 1; primes[1] = 2; primes[2] = 3;
         
+        for(int i = 5, j = 3; j <= n; i+=2){
+            if(isPrime(i)){
+                primes[j] = i;
+                j++;
+            }
+        }
+        
         return primes;
     }
     
-    public static boolean isPrime(int[] primes){
+    public static boolean isPrime(int candidate){
         return false;
     }
     
